ccd25216b3e7713100becad297b63a7708e2ee61
Gerson Lamela <gersonlamela7@gmail.com>
2023-05-19 21:59:55 +0100

    event #4

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -14,10 +14,14 @@ export function Profile() {
         alt=""
         className="h-10 w-10 rounded-full"
       />
-      <p className="max-w-[140px] text-sm leading-snug ">
+
+      <p className="max-w-[140px] text-sm leading-snug">
         {name}
-        <a href="" className="block text-red-400 hover:text-red-300">
-          Quero Sair
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:cursor-pointer hover:text-red-300"
+        >
+          Quero sair
         </a>
       </p>
     </div>
